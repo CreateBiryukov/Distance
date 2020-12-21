c30621170c438a8349a0431340985ee6129323ee
Alex Biryukov <birukov20011@gmail.com>
2020-12-21 14:47:39 +0300

    Buttons-animation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,1 @@
-var like=document.querySelector('.like-button');
-var count=0;
 
-like.addEventListener('click',function likeClicked(){
-    var newDiv = document.createElement("div");
-    newDiv.innerHTML = count;
-like.appendChild(newDiv);
-console.log(count++);
-})
